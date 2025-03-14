786bd35c939674bc4788b9331bdae564443e72cc
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2025-03-14 00:21:20 -0300

    test: :white_check_mark: Actualizar test de silo para que soporte el manejo de errores

--- crates/hardware/src/lib.rs
+++ crates/hardware/src/lib.rs
@@ -2,4 +2,5 @@ pub mod dosificador;
 pub mod silo;
 pub mod soplador;
 
+pub mod errors;
 pub mod traits;
--- crates/hardware/tests/silo.rs
+++ crates/hardware/tests/silo.rs
@@ -1,43 +1,125 @@
+use hardware::errors::SiloError;
 use hardware::silo::Silo;
 
 const CAPACIDAD_SILOS: u32 = 24000;
+const GRAMOS_PULSO: u32 = 25;
 
 #[cfg(test)]
-mod tests {
+mod silo {
     use super::*;
 
-    /// Prueba la creación de un nuevo dosificador y verifica sus valores iniciales.
-    #[test]
-    fn test_creacion_silo() {
-        let silo = Silo::new(CAPACIDAD_SILOS);
+    mod new {
+        use super::{Silo, CAPACIDAD_SILOS};
+        /// Prueba la creación de un nuevo dosificador y verifica sus valores iniciales.
+        #[test]
+        fn test_alimento_inicial() {
+            let silo = Silo::new(CAPACIDAD_SILOS);
+            assert_eq!(silo.get_alimento(), 0);
+        }
 
-        // Verificar valores iniciales
-        assert_eq!(silo.get_alimento(), 0);
-        assert_eq!(silo.get_historico(), 0);
+        #[test]
+        fn test_historico_inicial() {
+            let silo = Silo::new(CAPACIDAD_SILOS);
 
-        // Verificar que el UUID no sea el mismo en dos instancias diferentes
-        let id1 = silo.get_id();
-        let silo2 = Silo::new(24000);
-        let id2 = silo2.get_id();
-        assert_ne!(id1, id2);
+            assert_eq!(silo.get_historico(), 0);
+        }
+
+        mod id {
+            use super::{Silo, CAPACIDAD_SILOS};
+            use uuid::Uuid;
+
+            /// Prueba que la id sea unica
+            #[test]
+            fn test_unicidad() {
+                let silo = Silo::new(CAPACIDAD_SILOS);
+                // Verifica unicidad de id
+                let id1 = silo.get_id();
+                let id2 = Silo::new(CAPACIDAD_SILOS).get_id();
+                assert_ne!(id1, id2, "Las ID no son únicas");
+            }
+
+            /// Prueba que la id sea un UUID válido
+            #[test]
+            fn test_validez() {
+                let silo = Silo::new(CAPACIDAD_SILOS);
+                let id = silo.get_id();
+                let parsed_id = Uuid::parse_str(&id.to_string());
+                assert!(parsed_id.is_ok(), "El ID no es un UUID válido");
+            }
+
+            #[test]
+            fn tets_is_v4() {
+                let silo = Silo::new(CAPACIDAD_SILOS);
+                let id = silo.get_id();
+                let parsed_id = Uuid::parse_str(&id.to_string());
+                assert_eq!(
+                    parsed_id.unwrap().get_version(),
+                    Some(uuid::Version::Random),
+                    "La ID no es un UUID v4"
+                );
+            }
+        }
     }
 
     /// Prueba el setter `set_entrega` para modificar la capacidad de entrega del dosificador.
     #[test]
-    fn test_set_alimento() {
+    fn test_aumenta_alimento() {
         let mut silo = Silo::new(CAPACIDAD_SILOS);
 
         // Comprobamos que set alimento funciona bien
-        silo.set_alimento(10);
+        silo.set_alimento(10)
+            .expect("[test_aumenta_alimento]: No se ha podido actualizar [Linea 70]");
         assert_eq!(silo.get_alimento(), 10);
+    }
 
-        // Comprobamos que el historico aumenta
-        silo.set_alimento(11);
-        assert_eq!(silo.get_historico(), 11);
+    mod set_historico {
+        use super::{Silo, CAPACIDAD_SILOS};
+        #[test]
+        fn test_aumenta() {
+            let mut silo = Silo::new(CAPACIDAD_SILOS);
+            // Comprobamos que el historico aumenta
+            silo.set_alimento(11)
+                .expect("[test_aumenta]: No se ha actualizado el alimento");
+            assert_eq!(silo.get_historico(), 11);
+        }
+        #[test]
+        fn test_no_disminuye() {
+            let mut silo = Silo::new(CAPACIDAD_SILOS);
 
-        // Comprobamos que el historico no disminuye
-        silo.set_alimento(8);
-        assert_ne!(silo.get_historico(), 8);
+            assert_eq!(silo.get_historico(), 0); // Valor inicial
+
+            silo.set_alimento(11)
+                .expect("[test_no_disminuye]: No se ha actualizado el alimento [11]");
+            assert_eq!(silo.get_historico(), 11);
+            // Comprobamos que el historico no disminuye
+            silo.set_alimento(8)
+                .expect("[test_no_disminuye]: No se ha actualizado el alimento [8]");
+
+            assert_ne!(silo.get_historico(), 8);
+        }
+
+        #[test]
+        fn test_aumenta_varias_veces() {
+            let mut silo = Silo::new(CAPACIDAD_SILOS);
+
+            for i in 1..=100 {
+                silo.set_alimento(i).expect(
+                    "[test_aumenta_varias_veces]: No se ha podido actualizar alimento [Linea 105]",
+                );
+                assert_eq!(silo.get_historico(), i);
+            }
+
+            silo.set_alimento(0)
+                .expect("[test_aumenta_varias_veces]: No se ha podido actualiza [Linea 111]");
+            assert_eq!(silo.get_historico(), 100);
+
+            for i in 1..100 {
+                silo.set_alimento(i).expect(
+                    "[test_aumenta_varias_veces]: No se ha podido actualizar alimento [Linea 116]",
+                );
+                assert_eq!(silo.get_historico(), i + 100);
+            }
+        }
     }
 
     /// Verifica que el valor de `entrega` se obtiene correctamente.
@@ -47,54 +129,51 @@ mod tests {
         assert_eq!(silo.get_alimento(), 0); // Valor inicial
     }
 
-    /// Verifica que el valor de `estado` se obtiene correctamente.
-    #[test]
-    fn test_get_historico() {
-        let mut silo = Silo::new(CAPACIDAD_SILOS);
-        assert_eq!(silo.get_historico(), 0); // Valor inicial
+    // Revisar por que no pasó los tests
+    mod entregar_pulso {
+        use super::{Silo, SiloError, CAPACIDAD_SILOS, GRAMOS_PULSO};
 
-        silo.set_alimento(100); // Aumentamo 100 el historico (se supone)
-        assert_eq!(silo.get_historico(), 100);
+        /// Verificar que los pulsos se entregan
+        #[test]
+        fn test_actualiza_alimento() {
+            let mut silo = Silo::new(CAPACIDAD_SILOS);
 
-        silo.set_alimento(0); // Al vaciar el silo el historico deberia mantenerse en 100
-        assert_eq!(silo.get_historico(), 100);
+            silo.set_alimento(CAPACIDAD_SILOS);
 
-        silo.set_alimento(100); // Al volver a cargar 100 deberia sumarse con el historico y dar 200
-        assert_eq!(silo.get_historico(), 200);
-    }
+            silo.entregar_pulso(GRAMOS_PULSO);
+            assert_eq!(silo.get_alimento(), CAPACIDAD_SILOS - GRAMOS_PULSO);
 
-    /// Verifica que el UUID se obtiene correctamente y es un UUID válido.
-    #[test]
-    fn test_get_id() {
-        let silo = Silo::new(CAPACIDAD_SILOS);
-        let id = silo.get_id();
+            silo.entregar_pulso(CAPACIDAD_SILOS - GRAMOS_PULSO);
+            assert_eq!(silo.get_alimento(), 0);
+        }
 
-        // Verificar que el ID sea un UUID válido (simple check)
-        assert!(id.to_string().len() > 0);
-    }
+        #[test]
+        fn test_no_actualiza_historico() {
+            let mut silo = Silo::new(CAPACIDAD_SILOS);
 
-    /// Verificar que los pulsos se entregan
-    #[test]
-    fn test_entregar_pulso() {
-        let mut silo = Silo::new(CAPACIDAD_SILOS);
+            silo.set_alimento(CAPACIDAD_SILOS)
+                .expect("Ha sobrepasado la capacidad");
+            let historico = silo.get_historico();
 
-        silo.set_alimento(CAPACIDAD_SILOS);
+            silo.entregar_pulso(GRAMOS_PULSO).expect(&format!(
+                "El silo es incapaz de entregar {} gramos",
+                GRAMOS_PULSO
+            ));
+            assert_eq!(
+                silo.get_historico(),
+                historico,
+                "El valor de historico ha cambiado"
+            );
+        }
 
-        silo.entregar_pulso(5);
-        assert_eq!(silo.get_alimento(), CAPACIDAD_SILOS - 5);
+        #[test]
+        fn test_no_entrega_vacio() {
+            let mut silo = Silo::new(CAPACIDAD_SILOS);
 
-        silo.entregar_pulso(CAPACIDAD_SILOS - 5);
-        assert_eq!(silo.get_alimento(), 0);
-    }
+            let resultado = silo.entregar_pulso(GRAMOS_PULSO);
 
-    /// Verifica que los setters `set_alimento` y `entregar_pulso` se pueden encadenar correctamente.
-    #[test]
-    fn test_chain_setters() {
-        let mut silo = Silo::new(CAPACIDAD_SILOS);
-
-        // Probar el encadenamiento de setters
-        silo.set_alimento(5).entregar_pulso(1);
-
-        assert_eq!(silo.get_alimento(), 4);
+            // Compara el resultado con el error esperado dentro de SiloError
+            assert_eq!(resultado, Err(SiloError::SinAlimento));
+        }
     }
 }
